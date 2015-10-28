1cd3f3b1e55be4184ea2391d4fedca877de8cf82
Stefan Tanderup <Littlebir3d@gmail.com>
2015-10-28 14:13:44 +0100

    game container class created

---
+++ game classes/GameContainer/main.cpp
@@ -0,0 +1,38 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+class GameContainer
+{
+	float tempPlaceX, tempPlaceY;
+	int tempSizeX, tempSizeY;
+	int tempName;
+public:
+	void containerConstructer(int, int);
+	int sizeX() { return 100; }	//This is the number of the Horisontal size of the game board
+	int sizeY() { return 200; }	//This is the number of the Vertical size of the game board
+	int name = tempName; //Initialzed the ID in the constructer
+	float placeX = tempPlaceX;	//Initialzed the placeX in the constructer
+	float placeY = tempPlaceY;	//Initialzed the placeT in the constructer
+};
+
+void GameContainer::containerConstructer (int x, int y) {
+	tempSizeX = x;
+	tempSizeY = y;
+}
+
+int main(int, char)
+{
+	GameContainer gameInitialized;
+	gameInitialized.containerConstructer(0, 0);
+	//Returns the sizeX and sizeY
+	cout << "sizeX: " << gameInitialized.sizeX() << "\nsizeY: " << gameInitialized.sizeY() << endl;
+	cout << "Before name has a value: " << gameInitialized.name << "\n";
+	gameInitialized.name = 100;
+	cout << "After name has a been given a value of 100: " << gameInitialized.name;
+	return(0);
+}
