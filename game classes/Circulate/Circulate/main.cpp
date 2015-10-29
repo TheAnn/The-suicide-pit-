7042449fd17bc020b51722a2f4f798e6f88fbe1e
Stefan Tanderup <Littlebir3d@gmail.com>
2015-10-29 13:56:19 +0100

    More edited and better

--- game classes/Circulate/Circulate/main.cpp
+++ game classes/Circulate/Circulate/main.cpp
@@ -1,17 +1,16 @@
 #include "opencv2/opencv.hpp"
-#include <conio.h>
+#include <iostream>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
 
 using namespace cv;
+using namespace std;
 
-void rotate(cv::Mat& src, double rotateAngle, cv::Mat& dst)
+void rotate(Mat& src, double rotateAngle, Mat& dst, int x, int y)
 {
-	int len = max(src.cols, src.rows);
-	cv::Point2f pt(len / 2., len / 2.);
-	cv::Mat r = cv::getRotationMatrix2D(pt, rotateAngle, 1.0);
-	cv::warpAffine(src, dst, r, Size(len, len));
+	Mat r = cv::getRotationMatrix2D(Point(x, y), rotateAngle, 1.0);
+	warpAffine(src, dst, r, Size(500, 500));
 }
 
 int main(int, char)
@@ -20,6 +19,7 @@ int main(int, char)
 	int y = 100;
 	int rotateX, rotateY;
 	int angle = 2 * M_PI / 1;
+	float rotateAngle;
 	int radius = 50;
 	float speed = 0;
 	float rotateSpeed = 0;
@@ -40,11 +40,7 @@ int main(int, char)
 		Mat textImg = Mat::zeros(image.rows, image.cols, image.type());
 		circle(image, Point(x, y), 50, Scalar(255, 0, 0), 1, 1);
 		putText(textImg, text, Point(x - textSize.width / 2, y + textSize.height / 2), fontFace, fontScale, Scalar::all(255), 1, 8);
-
-
-
-		putText(textImg, text, Point(100, 100), fontFace, fontScale, Scalar::all(255), 1, 8);
-		rotate(textImg, rotateSpeed, textImg);
+		rotate(textImg, rotateSpeed, textImg, x, y);
 
 		image = image + textImg;
 		imshow("cool pic", image);
