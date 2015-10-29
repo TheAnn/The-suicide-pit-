6f52b4bf9764e5158b1007828e3ff260a6cf0241
Stefan Tanderup <Littlebir3d@gmail.com>
2015-10-29 18:12:02 +0100

    Numbers are added in an array now :D

--- game classes/Circulate/Circulate/main.cpp
+++ game classes/Circulate/Circulate/main.cpp
@@ -7,7 +7,7 @@
 using namespace cv;
 using namespace std;
 
-void rotate(Mat& src, double rotateAngle, Mat& dst, int x, int y)
+void rotate(Mat src, double rotateAngle, Mat dst, int x, int y)
 {
 	Mat r = cv::getRotationMatrix2D(Point(x, y), rotateAngle, 1.0);
 	warpAffine(src, dst, r, Size(500, 500));
@@ -15,39 +15,63 @@ void rotate(Mat& src, double rotateAngle, Mat& dst, int x, int y)
 
 int main(int, char)
 {
-	int x = 100;
-	int y = 100;
-	int rotateX, rotateY;
-	int angle = 2 * M_PI / 1;
-	float rotateAngle;
+	//Initializing all the variables
+	int x, y;
+	float angle;
 	int radius = 150;
 	float speed = 0;
 	float rotateSpeed = 0;
+	int answers[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+	int answersSize;
+
+	answersSize = sizeof(answers) / sizeof(*answers);
+	angle = 2*M_PI / answersSize;
+	x = radius*sin(angle * answersSize) + 500 / 2;
+	y = radius*cos(angle * answersSize) + 500 / 2;
+	
+	
 
 	//creating the text
-	String text = "3";
 	int fontFace = FONT_HERSHEY_SIMPLEX;
 	double fontScale = 0.7;
 	int thickness = 3;
 	int baseline = 0;
-	Size textSize = getTextSize(text, fontFace, fontScale, thickness, &baseline);
-	baseline += thickness;
+
 
 
 	for (;;)
 	{
+		//Creating the matrix for the source image (The one getting shown)
 		Mat image = Mat::zeros(500, 500, CV_8UC3);
-		Mat textImg = Mat::zeros(image.rows, image.cols, image.type());
-		circle(image, Point(x, y), 20, Scalar(255, 0, 0), 1, 1);
-		putText(textImg, text, Point(x - textSize.width / 2, y + textSize.height / 2), fontFace, fontScale, Scalar::all(255), 1, 8);
-		rotate(textImg, rotateSpeed, textImg, x, y);
 
-		image = image + textImg;
-		imshow("cool pic", image);
-		x = radius*sin(angle * 1 + speed) + 500 / 2;
-		y = radius*cos(angle * 1 + speed) + 500 / 2;
+		for (int i = 0; i < answersSize; i++)
+		{
+			//Create the number inside the circle
+			String text = to_string(answers[i]);
+			Size textSize = getTextSize(text, fontFace, fontScale, thickness, &baseline);
+			baseline += thickness;
+
+			//Creating matrixes for each of the different answers, to make them "rotateable". Currently not in use
+			Mat textImg = Mat::zeros(image.rows, image.cols, image.type());
+
+			//Declaring the position of the answer
+			x = radius*sin(angle * i + speed) + 500 / 2;
+			y = radius*cos(angle * i + speed) + 500 / 2;
+			
+			//Creating the numbers to be shown
+			circle(image, Point(x, y), 20, Scalar::all(255), 1, 1);
+			putText(textImg, text, Point(x - textSize.width / 2, y + textSize.height / 2), fontFace, fontScale, Scalar::all(255), 1, 8);
+			//rotate(textImg, rotateSpeed, textImg, x, y);
+
+			//Adds the new text image to the source image
+			image = image + textImg;
+		}
+		
+		
 		speed += 0.05;
 		rotateSpeed += 2.8;
+		
+		imshow("cool pic", image);
 		waitKey(0);
 	}
 	return 0;
