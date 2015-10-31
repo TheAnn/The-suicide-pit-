1a5230812029e35086648736274551a67c5a00d4
Stefan Tanderup <Littlebir3d@gmail.com>
2015-10-31 13:36:01 +0100

    Added drag&drop functions

---
+++ game classes/Grab/Grab/main.cpp
@@ -0,0 +1,15 @@
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+int main(int, char)
+{
+	int collide = 1; //The collide should come from the Hand class
+	bool holding = false;
+
+
+	if (collide % 2 == 1){
+		//id.answer.posX = hand.x
+		//id.answer.posY = hand.y
+	}
+}
